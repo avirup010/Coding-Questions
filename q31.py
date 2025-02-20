be2e6f31c7048c01f8167a27f226569175ca15fd
Avirup Ghosh <109033643+avirup010@users.noreply.github.com>
2025-02-21 00:50:22 +0530

    Create q31.py

---
+++ q31.py
@@ -0,0 +1,7 @@
+#Polar Coordinates in Python
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+
+n = input()
+print(abs(complex(n)))
+print(cmath.phase(complex(n)))
