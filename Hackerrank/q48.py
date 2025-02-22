98debe0bbb3d5b5d499e541dd35a54a889987da6
Avirup Ghosh <109033643+avirup010@users.noreply.github.com>
2025-02-22 15:29:45 +0530

    Create q48.py

---
+++ Hackerrank/q48.py
@@ -0,0 +1,10 @@
+#Collections.deque() 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+
+D = deque()
+
+for _ in range(int(input())):
+    oper, val, *args = input().split() + ['']
+    eval(f'D.{oper} ({val})')
+print(*D)
