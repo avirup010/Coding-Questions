4f7ee1f2353100a92c02ab8067973a02ee9fceb4
Avirup Ghosh <109033643+avirup010@users.noreply.github.com>
2025-02-22 15:38:52 +0530

    Create q59.py

---
+++ Hackerrank/python/q59.py
@@ -0,0 +1,10 @@
+#Set.difference() Operation
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+_ = int(input())
+SET_N = set(map(int, input().split()))
+
+_ = int(input())
+SET_B = set(map(int, input().split()))
+
+NEW_SET = SET_N.difference(SET_B)
+print(len(NEW_SET))
