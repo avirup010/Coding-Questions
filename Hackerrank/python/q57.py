97086df27e6c8b77201e284cd54707a3d678f1df
Avirup Ghosh <109033643+avirup010@users.noreply.github.com>
2025-02-22 15:37:27 +0530

    Create q57.py

---
+++ Hackerrank/python/q57.py
@@ -0,0 +1,13 @@
+#Power – Mod Power
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+c = math.pow(a, b)
+d = c%m
+
+print(int(c))
+print(int(d))
