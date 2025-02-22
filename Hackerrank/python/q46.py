b1fa4d4c90f5c227e80b7335ed187bcf874d5837
Avirup Ghosh <109033643+avirup010@users.noreply.github.com>
2025-02-22 15:27:53 +0530

    Create q46.py

---
+++ Hackerrank/python/q46.py
@@ -0,0 +1,14 @@
+#Word Order
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import Counter
+
+N = int(input())
+LIST = []
+
+for i in range(N):
+    LIST.append(input().strip())
+
+COUNT = Counter(LIST)
+
+print(len(COUNT))
+print(*COUNT.values())
